53b9fe05fc4219cc3459848ba9284aad1584c3bd
“Genaro <genaro.coronel561@gmail.com>
2023-07-31 17:32:28 -0500

    script changes

--- scripts/create_tags_and_release_notes.js
+++ scripts/create_tags_and_release_notes.js
@@ -1,27 +1,27 @@
-// scripts/create_tags_and_release_notes.js
-
 const fs = require('fs');
 
 function generateVersionTag(ticketNumbers) {
   return `v1.0.0-clickup-${ticketNumbers.join('-')}`;
 }
 
-function generateReleaseNotes(ticketNumbers) {
+function generateReleaseNotes(ticketNumbers, firstLines, clickUpLinks) {
   // In a real scenario, you might want to fetch additional details for each ticket
   // from ClickUp API or other sources to generate comprehensive release notes.
-  // For this example, we'll generate simple release notes with ticket numbers.
-  const releaseNotesContent = ticketNumbers.map((ticket) => `- Fix issue #${ticket}`).join('\n');
+  // For this example, we'll generate simple release notes with ticket numbers and ClickUp links.
+  const releaseNotesContent = ticketNumbers.map((ticket, index) => `- Fix issue #${ticket}\n  ${firstLines[index]}\n  ClickUp Ticket: ${clickUpLinks[index]}`).join('\n');
   return `# Release Notes\n\n${releaseNotesContent}`;
 }
 
 async function run() {
   try {
     const ticketNumbers = fs.readFileSync('clickup-tickets.txt', 'utf8').trim().split('\n');
+    const prFirstLines = fs.readFileSync('pr-first-lines.txt', 'utf8').trim().split('\n');
+    const clickUpLinks = fs.readFileSync('clickup-links.txt', 'utf8').trim().split('\n');
 
     const versionTag = generateVersionTag(ticketNumbers);
     fs.writeFileSync('version.txt', versionTag);
 
-    const releaseNotes = generateReleaseNotes(ticketNumbers);
+    const releaseNotes = generateReleaseNotes(ticketNumbers, prFirstLines, clickUpLinks);
     fs.writeFileSync('release-notes.md', releaseNotes);
   } catch (error) {
     console.error('Error occurred while creating tags and release notes:', error);
--- scripts/extract_clickup_tickets.js
+++ scripts/extract_clickup_tickets.js
@@ -1,33 +1,42 @@
-// scripts/extract_clickup_tickets.js
-
 const fs = require('fs');
 
 function extractClickUpTickets(prDescription, prTitle) {
   const clickUpTicketRegex = /#(\d+)/g;
-  const descriptionMatches = prDescription.match(clickUpTicketRegex) || 
-[];
+  const descriptionMatches = prDescription.match(clickUpTicketRegex) || [];
   const titleMatches = prTitle.match(clickUpTicketRegex) || [];
-  const allMatches = [...new 
-Set(descriptionMatches.concat(titleMatches))]; // Remove duplicate matches
+  const allMatches = [...new Set(descriptionMatches.concat(titleMatches))]; // Remove duplicate matches
 
   return allMatches.map((match) => match.replace('#', ''));
 }
 
+function extractFirstLines(prDescription) {
+  // Extract the first line of the PR description
+  const firstLines = prDescription.trim().split('\n').map((line) => line.trim()).filter(Boolean);
+  return firstLines;
+}
+
+function generateClickUpLinks(ticketNumbers) {
+  // Assuming your ClickUp tasks are hosted at 'https://app.clickup.com/t/'
+  const clickUpBaseUrl = 'https://app.clickup.com/t/';
+  const clickUpLinks = ticketNumbers.map((ticket) => `${clickUpBaseUrl}${ticket}`);
+  return clickUpLinks;
+}
+
 async function run() {
   try {
-    const prDescription = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 
-'utf8');
-    const prTitle = process.env.GITHUB_HEAD_REF || ''; 
-    // Assumes the PR title is available as the branch name
+    const prDescription = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
+    const prTitle = process.env.GITHUB_HEAD_REF || ''; // Assumes the PR title is available as the branch name
     const ticketNumbers = extractClickUpTickets(prDescription, prTitle);
+    const firstLines = extractFirstLines(prDescription);
+    const clickUpLinks = generateClickUpLinks(ticketNumbers);
 
     fs.writeFileSync('clickup-tickets.txt', ticketNumbers.join('\n'));
+    fs.writeFileSync('pr-first-lines.txt', firstLines.join('\n'));
+    fs.writeFileSync('clickup-links.txt', clickUpLinks.join('\n'));
   } catch (error) {
-    console.error('Error occurred while extracting ClickUp tickets:', 
-error);
+    console.error('Error occurred while extracting ClickUp tickets:', error);
     process.exit(1);
   }
 }
 
 run();
-
